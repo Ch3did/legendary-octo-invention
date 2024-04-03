2c726ef85bdbf47c2e15bb911edbcacecf48ad04
Ch3did <phchedidk@gmail.com>
2024-04-03 14:11:22 -0300

    feat: added logic for index page

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-from flask import Flask, render_template  # Importa a biblioteca
+from flask import Flask, render_template,  request
 
-app = Flask(__name__, template_folder="template")  # Inicializa a aplicação
+app = Flask(__name__, template_folder="template")
 
-
-@app.route("/")  # Cria uma rota
+@app.route("/", methods=['GET', 'POST'])
 def auth():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        request.form["usuário"]
+        request.form["senha"]
 
 
-@app.route("/client/")  # Cria uma rota
+@app.route("/client/")
 def client():
     return render_template("pag2Cliente.html")
 
 
-@app.route("/table/")  # Cria uma rota
+@app.route("/table/")
 def table():
     return render_template("tabela.html")
 
 
-@app.route("/boletim1/")  # Cria uma rota
+@app.route("/boletim1/")
 def boletim1():
     return render_template("Boletim1.html")
 
 
-@app.route("/boletim2/")  # Cria uma rota
+@app.route("/boletim2/")
 def boletim2():
     return render_template("Boletim2.html")
 
